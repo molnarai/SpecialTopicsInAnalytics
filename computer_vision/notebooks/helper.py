1b182757332a25cdecdb41774acf51a270dfd77f
Peter Molnar <pmolnar@gsu.edu>
2025-10-23 00:00:03 +0000

    update

--- computer_vision/notebooks/helper.py
+++ computer_vision/notebooks/helper.py
@@ -1,6 +1,6 @@
 import cv2
 
-def load_and_scale(file_name, size = (400, 300)):
+def load_and_scale(file_name: str, size = (400, 300)):
     img = cv2.imread(file_name)
     img = cv2.resize(img, size)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
